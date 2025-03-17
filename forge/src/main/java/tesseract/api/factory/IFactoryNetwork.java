1c9068b1e0212df8495454833de9e5cc2ff2c839
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2025-03-16 21:26:19 -0400

    added basic factory api

---
+++ forge/src/main/java/tesseract/api/factory/IFactoryNetwork.java
@@ -0,0 +1,20 @@
+package tesseract.api.factory;
+
+import java.util.Collection;
+
+/**
+ * A factory network is a logical group of factory elements.
+ * You usually want to extend {@link StandardFactoryNetwork}, not this.
+ */
+public interface IFactoryNetwork<TSelf extends IFactoryNetwork<TSelf, TElement, TGrid>, TElement extends IFactoryElement<TElement, TSelf, TGrid>, TGrid extends IFactoryGrid<TGrid, TElement, TSelf>> {
+
+    public void addElement(TElement element);
+
+    public void removeElement(TElement element);
+
+    public default void onNetworkRemoved() {
+
+    }
+
+    public Collection<TElement> getElements();
+}
