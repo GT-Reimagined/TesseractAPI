d1e5fbb1b04d276f3129de69c6c290a0aeaae84f
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2025-05-08 22:09:29 -0400

    added TCAPABILITY to INotableFactoryElement

--- common/src/main/java/tesseract/factory/INotableFactoryElement.java
+++ common/src/main/java/tesseract/factory/INotableFactoryElement.java
@@ -4,7 +4,9 @@ import it.unimi.dsi.fastutil.Pair;
 
 import java.util.List;
 
-public interface INotableFactoryElement<TSelf extends IFactoryElement<TSelf, TNetwork, TGrid> & INotableFactoryElement<TSelf, TRoutingInfo, TNetwork, TGrid>, TRoutingInfo extends IRoutingInfo<TRoutingInfo>, TNetwork extends IFactoryNetwork<TNetwork, TSelf, TGrid>, TGrid extends IFactoryGrid<TGrid, TSelf, TNetwork>> {
+public interface INotableFactoryElement<TSelf extends IFactoryElement<TSelf, TNetwork, TGrid> & INotableFactoryElement<TSelf, TRoutingInfo, TCAPABILITY, TNetwork, TGrid>, TRoutingInfo extends IRoutingInfo<TRoutingInfo>, TCAPABILITY, TNetwork extends IFactoryNetwork<TNetwork, TSelf, TGrid>, TGrid extends IFactoryGrid<TGrid, TSelf, TNetwork>> {
     /** Exactly the same as getConnections, but with some routing metadata. */
     List<Pair<TSelf, TRoutingInfo>> getRoutedNeighbours();
+
+    TCAPABILITY getCapability(byte direction);
 }
