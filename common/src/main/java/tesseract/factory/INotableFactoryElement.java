47fef1b15101dfb9a66d66d00b025b94f94a307f
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2025-05-11 12:53:56 -0400

    made sure INotableFactoryElement has to also extend IFactoryElement

--- common/src/main/java/tesseract/factory/INotableFactoryElement.java
+++ common/src/main/java/tesseract/factory/INotableFactoryElement.java
@@ -4,7 +4,7 @@ import it.unimi.dsi.fastutil.Pair;
 
 import java.util.List;
 
-public interface INotableFactoryElement<TSelf extends INotableFactoryElement<TSelf, TRoutingInfo, TElement, TNetwork, TGrid>, TRoutingInfo extends IRoutingInfo<TRoutingInfo>, TElement extends IFactoryElement<TElement, TNetwork, TGrid>, TNetwork extends IFactoryNetwork<TNetwork, TElement, TGrid>, TGrid extends IFactoryGrid<TGrid, TElement, TNetwork>> extends IFactoryElement<TElement, TNetwork, TGrid> {
+public interface INotableFactoryElement<TSelf extends INotableFactoryElement<TSelf, TRoutingInfo, TElement, TNetwork, TGrid> & IFactoryElement<TElement, TNetwork, TGrid>, TRoutingInfo extends IRoutingInfo<TRoutingInfo>, TElement extends IFactoryElement<TElement, TNetwork, TGrid>, TNetwork extends IFactoryNetwork<TNetwork, TElement, TGrid>, TGrid extends IFactoryGrid<TGrid, TElement, TNetwork>> extends IFactoryElement<TElement, TNetwork, TGrid> {
     /** Exactly the same as getConnections, but with some routing metadata. */
     List<Pair<TSelf, TRoutingInfo>> getRoutedNeighbours();
 }
