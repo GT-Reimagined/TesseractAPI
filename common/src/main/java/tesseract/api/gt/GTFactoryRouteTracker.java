a2a3b6b088b43d383076d9141d18897d2d696cb6
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2025-06-21 23:21:56 -0400

    added insert method to GTFactoryRouteTracker

--- common/src/main/java/tesseract/api/gt/GTFactoryRouteTracker.java
+++ common/src/main/java/tesseract/api/gt/GTFactoryRouteTracker.java
@@ -1,10 +1,12 @@
 package tesseract.api.gt;
 
 import it.unimi.dsi.fastutil.Pair;
+import tesseract.TesseractCapUtils;
 import tesseract.factory.IFactoryPath;
 import tesseract.factory.standard.StandardFactoryRouteTracker;
 
 import java.util.List;
+import java.util.Optional;
 
 public class GTFactoryRouteTracker extends StandardFactoryRouteTracker<GTRoutingInfo, IGTNodeBlock, IGTCable, GTFactoryNetwork, GTFactoryGrid> {
     @Override
@@ -12,8 +14,22 @@ public class GTFactoryRouteTracker extends StandardFactoryRouteTracker<GTRouting
         return new GTFactoryPath(pair.first(), pair.second());
     }
 
+    public long insertEU(IGTNodeBlock source, long eu, boolean simulate){
+        long inserted = 0;
+        for (IFactoryPath<GTRoutingInfo, IGTNodeBlock, IGTCable, GTFactoryNetwork, GTFactoryGrid> path : getPaths(source)) {
+            if (path.getDestination().getBlockEntity() != null){
+                Optional<IEnergyHandler> handler = TesseractCapUtils.INSTANCE.getEnergyHandler(path.getDestination().getBlockEntity(), path.getRoutingInfo().side());
+                long finalEu = eu;
+                long insert = handler.map(h -> h.insertEu(finalEu, simulate)).orElse(0L);
+                eu -= insert;
+                inserted += insert;
+            }
+        }
+        return inserted;
+    }
+
     @Override
     public int sort(IFactoryPath<GTRoutingInfo, IGTNodeBlock, IGTCable, GTFactoryNetwork, GTFactoryGrid> a, IFactoryPath<GTRoutingInfo, IGTNodeBlock, IGTCable, GTFactoryNetwork, GTFactoryGrid> b) {
-        return 0;
+        return a.getRoutingInfo().actualLoss() > b.getRoutingInfo().actualLoss() ? 1 : -1;
     }
 }
