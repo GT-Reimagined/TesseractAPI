20ac100470cd7e25997ad9eba089534834bd40de
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2025-06-22 22:16:13 -0400

    added cableIsActive map to GTFactoryNetwork

--- common/src/main/java/tesseract/api/gt/GTFactoryNetwork.java
+++ common/src/main/java/tesseract/api/gt/GTFactoryNetwork.java
@@ -1,9 +1,15 @@
 package tesseract.api.gt;
 
+import it.unimi.dsi.fastutil.longs.LongOpenHashSet;
+import it.unimi.dsi.fastutil.longs.LongSet;
+import it.unimi.dsi.fastutil.objects.Object2ObjectLinkedOpenHashMap;
+import it.unimi.dsi.fastutil.objects.Object2ObjectMap;
+import net.minecraft.resources.ResourceLocation;
 import tesseract.factory.IRouteTracker;
 import tesseract.factory.standard.StandardFactoryNetwork;
 
 public class GTFactoryNetwork extends StandardFactoryNetwork<GTFactoryNetwork, IGTCable, IGTNode, GTRoutingInfo, GTFactoryGrid> {
+    public final Object2ObjectMap<ResourceLocation, LongSet> cableIsActive = new Object2ObjectLinkedOpenHashMap<>();
     @Override
     protected IRouteTracker<GTRoutingInfo, IGTNode, IGTCable, GTFactoryNetwork, GTFactoryGrid> createRouteTracker() {
         return new GTFactoryRouteTracker();
