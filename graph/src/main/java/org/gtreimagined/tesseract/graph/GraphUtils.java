54df0ae422edb1cf6ffae3c1ed9ba8dbb734aac3
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2025-07-23 13:51:44 -0400

    removed graph folder

--- graph/src/main/java/org/gtreimagined/tesseract/graph/GraphUtils.java
+++
@@ -1,9 +0,0 @@
-package org.gtreimagined.tesseract.graph;
-
-import java.util.ServiceLoader;
-
-public interface GraphUtils {
-    GraphUtils INSTANCE =  ServiceLoader.load(GraphUtils.class).findFirst().orElseThrow(() -> new IllegalStateException("No implementation of GraphUtils found"));
-
-    boolean isDevEnvironment();
-}
--- graph/src/main/java/org/gtreimagined/tesseract/graph/RoutedNode.java
+++
@@ -1,4 +0,0 @@
-package org.gtreimagined.tesseract.graph;
-
-public record RoutedNode<TElement, TRouteInfo>(TElement element, TRouteInfo routeInfo) {
-}
