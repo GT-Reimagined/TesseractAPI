7c5ea8218040fc2e6034234bfc0b5bb3df85e07b
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2025-09-25 15:57:09 -0400

    Revert "trying moving the amp reset logic to the cables themselves"
    This reverts commit 5062f4f9662280450f7a80e4a9c140301fc72f33.

--- common/src/main/java/org/gtreimagined/tesseract/api/eu/EUGrid.java
+++ common/src/main/java/org/gtreimagined/tesseract/api/eu/EUGrid.java
@@ -9,4 +9,12 @@ public class EUGrid extends StandardGrid<EUGrid, IEUCable, IEUNode, EURoutingInf
     protected EUNetwork createNetwork() {
         return new EUNetwork();
     }
+
+    @Override
+    public void tick() {
+        super.tick();
+        for (IEUCable cable : vertices){
+            cable.setHolder(EUHolder.create(cable, 0));
+        }
+    }
 }
